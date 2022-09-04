ef6e0264f826c362406d5221058503735e849cd0
Abdellatif Laghjaj <abdelatiflaghjaj@gmail.com>
2022-09-04 13:09:19 -0700

    improve logic

--- js/script.js
+++ js/script.js
@@ -2,32 +2,24 @@ const input = document.querySelector('input');
 const downloadButton = document.querySelector('button');
 
 downloadButton.addEventListener('click', () => {
-    if(checkFile(input.value !== '')){
-        downloadFile(input.value);
-    }else{
-        alert('Please enter a link');
-    }
+    downloadButton.innerHTML = 'Downloading...';
+    downloadFile(input.value);
 });
 
 
 //download file
-function downloadFile(url){
-    fetch(url).then(res => {
-        if(res.ok){
-            res.blob()
-        }else{
-            alert('file does not exist');
-        }
-    })
-    .then(blob => {
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.style.display = 'none';
-        a.href = url;
-        a.download = 'myFile';
-        document.body.appendChild(a);
-        a.click();
-        a.remove();
-        URL.revokeObjectURL(url);
-    })
+function downloadFile(url) {
+    fetch(url).then(res => res.blob())
+        .then(blob => {
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.style.display = 'none';
+            a.href = url;
+            a.download = url.replace(/^.*[\\\/]/, '');
+            document.body.appendChild(a);
+            a.click();
+            a.remove();
+            URL.revokeObjectURL(url);
+            downloadButton.innerHTML = 'download';
+        })
 }
